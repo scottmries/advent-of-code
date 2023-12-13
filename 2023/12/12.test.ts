b438906d60985656830b63db66fdd4b521d045e8
Scott Ries <scottmries@gmail.com>
2023-12-13 04:58:46 -0500

    Wow, Day 12. Now that was hard.

--- 2023/12/12.test.ts
+++ 2023/12/12.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "bun:test";
-import solution, { solve } from './12';
+import solution, { solve1 } from './12';
 
 const example = [
     '???.### 1,1,3',
@@ -12,37 +12,37 @@ const example = [
 
 describe("find correct arrangements with", () => {
     test("hashes", () => {
-        expect(solve('# 1')).toBe(1);
-        expect(solve('# 2')).toBe(0);
-        expect(solve('## 2')).toBe(1);
-        expect(solve('## 3')).toBe(0);
+        expect(solve1('# 1')).toBe(1);
+        expect(solve1('# 2')).toBe(0);
+        expect(solve1('## 2')).toBe(1);
+        expect(solve1('## 3')).toBe(0);
     })
     test("dots", () => {
-        expect(solve('. 1')).toBe(0);
-        expect(solve('. 2')).toBe(0);
+        expect(solve1('. 1')).toBe(0);
+        expect(solve1('. 2')).toBe(0);
     })
     test("mixed hashes and dots", () => {
-        expect(solve('.# 1')).toBe(1);
-        expect(solve('#. 1')).toBe(1);
-        expect(solve('## 1')).toBe(0);
-        expect(solve('#.# 1,1')).toBe(1);
-        expect(solve('#..# 1,1')).toBe(1);
-        expect(solve('##.### 2,3')).toBe(1);
+        expect(solve1('.# 1')).toBe(1);
+        expect(solve1('#. 1')).toBe(1);
+        expect(solve1('## 1')).toBe(0);
+        expect(solve1('#.# 1,1')).toBe(1);
+        expect(solve1('#..# 1,1')).toBe(1);
+        expect(solve1('##.### 2,3')).toBe(1);
     })
     test("only question marks", () => {
-        expect(solve('? 1')).toBe(1);
-        expect(solve('? 2')).toBe(0);
-        expect(solve('?? 1')).toBe(2);
-        expect(solve('??? 1')).toBe(3);
-        expect(solve('??? 2')).toBe(2);
-        expect(solve('??? 1,1')).toBe(1);
+        expect(solve1('? 1')).toBe(1);
+        expect(solve1('? 2')).toBe(0);
+        expect(solve1('?? 1')).toBe(2);
+        expect(solve1('??? 1')).toBe(3);
+        expect(solve1('??? 2')).toBe(2);
+        expect(solve1('??? 1,1')).toBe(1);
     })
 })
 
 describe("example", () => {
     ([1, 4, 1, 1, 4, 10]).map((solution, index) => {
         test(`${index}'s arrangements are correct`, () => {
-            expect(solve(example[index])).toBe(solution)
+            expect(solve1(example[index])).toBe(solution)
         })
     })
     test('solution is correct', () => {
--- 2023/12/12.ts
+++ 2023/12/12.ts
@@ -2,11 +2,12 @@ const { readFileSync } = require('fs')
 
 const lines = readFileSync('./12/12.txt').toString().split("\n")
 
-export const solve = (line:string): number => {
-    console.log('solving', line)
+let solved = {}
+
+export const solve1 = (line:string): number => {
     let record = line.split(" ")[0]
     let groups = line.split(" ")[1].split(",").map(el => Number(el))
-    return getArrangements(record, groups)
+    return getArrangements({record, groups})
 }
 
 export const solve2 = (line:string): number => {
@@ -14,57 +15,68 @@ export const solve2 = (line:string): number => {
     const groupsString = line.split(" ")[1]
     record = Array(5).fill(record).join("?")
     const groups = Array(5).fill(groupsString).join(",").split(",").map(el => Number(el))
-    console.log('solving', record, groups.join(","))
-    return getArrangements(record, groups)
+    return getArrangements({record, groups})
 }
 
-export const getArrangements = (record: string, groups: number[]): number => {
-    if (groups.length === 0 && record.indexOf('#') > -1) {
-        return 0
-    }
-    if (groups.length === 1 && record.lastIndexOf('#') - record.indexOf('#') > groups[0]) {
-        return 0
-    }
-    const groupsSum = groups.reduce((a, c) => a + c, 0)
-    if (groupsSum + groups.length - 1 > record.length) {
-        return 0
-    }
-    if (record.length < groups[0]) {
-        return 0
-    }
-    if (/^\./.test(record)) {
-        record = record.substring(1)
-    }
-    if (record.length === 0) {
-        return groups.length === 0 ? 1 : 0
-    }
-    if (/^#/.test(record)) {
-        if (groups.length === 0) {
+export const getArrangements = (line: {record:string, groups:number[]}): number => {
+    const lineString = (record:string, groups:number[]): string => [record, groups.join(",")].join(" ")
+    let count = 0
+    let lines = [line]
+    
+
+    const solve = (record: string, groups: number[]): number => {
+        if (typeof solved[lineString(record, groups)] !== 'undefined') {
+            return solved[lineString(record, groups)]
+        }
+        if (record.length < groups.reduce((a, c) => a + c, 0) + groups.length - 1) {
+            solved[lineString(record, groups)] = 0
             return 0
         }
-        const groupRegex = new RegExp(`^[#?]{${groups[0]}}`)
-        if (groupRegex.test(record)) {
-            record = record.substring(groups[0])
-            if (/^#/.test(record)) {
-                return 0
+        while (record[0] === '.') {
+            if (typeof solved[lineString(record.substring(1), groups)] !== 'undefined') {
+                solved[lineString(record, groups)] = solved[lineString(record.substring(1), groups)]
             }
-            groups = groups.slice(1)
-            if (record.length) {
-                record = '.' + record.substring(1)
-            }
-        } else {
-            return 0
+            record = record.substring(1)
         }
+        if (record.length === 0) {
+            let result = 0
+            if (groups.length === 0) {
+                result = 1
+            }
+            solved[lineString(record, groups)] = result
+            return result
+        }
+        const hashRe = new RegExp(`^#[#?]{${groups[0] - 1}}`)
+        if (record[0] === '#'){
+            if(hashRe.test(record)) {
+                let nextRecord = record.substring(groups[0])
+                if (nextRecord[0] === '#') {
+                    solved[lineString(record, groups)] = 0
+                    return 0
+                }
+                const nextSolve = solve(nextRecord.substring(1), groups.slice(1))
+                solved[lineString(record, groups)] = nextSolve
+                return nextSolve
+            }
+            solved[lineString(record, groups)] = 0
+            return solved[lineString(record, groups)]
+        }
+        if (record[0] === '?') {
+            const nextSolve = solve('.' + record.substring(1), groups) + solve('#' + record.substring(1), groups)
+            solved[lineString(record, groups)] = nextSolve
+            return nextSolve
+        }
+        return -1
     }
-    if (/^\?/.test(record)) {
-        return getArrangements('.' + record.substring(1), groups) + getArrangements('#' + record.substring(1), groups)
-    }
-    return getArrangements(record, groups)
+    const {record, groups} = line
+    count = solve(record, groups)
+    
+    return count
 }
 
 export const solution = (input: string[]): {solution1: number, solution2: number} => {
     return {
-        solution1: input.reduce((acc, line) => acc + solve(line), 0),
+        solution1: input.reduce((acc, line) => acc + solve1(line), 0),
         solution2: input.reduce((acc, line) => acc + solve2(line), 0),
     }
 }
