a77e647c2bcca8bf55f50eb04ebf548c346b522d
Scott Ries <scottmries@gmail.com>
2023-12-14 10:44:23 -0500

    Solutions for Day 13.

--- 2023/13/13.test.ts
+++ 2023/13/13.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "bun:test";
-import solve, { rotate, solution, matrices } from './13';
+import solve, { getFlipLocations, rotate, solution, matrices } from './13';
 
 const example1 = [
     "#.##..##.",
@@ -33,13 +33,79 @@ const example1Rotated = [
     "..##...",
 ]
 
+const smallExample = [
+    "..#.##..",
+    ".#....##",
+    "..#####."
+]
+
+const exampleSet = [
+    [
+        '#.##..##.',
+        '..#.##.#.',
+        '##......#',
+        '##......#',
+        '..#.##.#.',
+        '..##..##.',
+        '#.#.##.#.'
+    ],[
+        '#...##..#',
+        '#....#..#',
+        '..##..###',
+        '#####.##.',
+        '#####.##.',
+        '..##..###',
+        '#....#..#'
+    ],[
+        '.#.##.#.#',
+        '.##..##..',
+        '.#.##.#..',
+        '#......##',
+        '#......##',
+        '.#.##.#..',
+        '.##..##.#',
+    ],[
+        '#..#....#',
+        '###..##..',
+        '.##.#####',
+        '.##.#####',
+        '###..##..',
+        '#..#....#',
+        '#..##...#'
+    ],[
+        '#.##..##.',
+        '..#.##.#.',
+        '##..#...#',
+        '##...#..#',
+        '..#.##.#.',
+        '..##..##.',
+        '#.#.##.#.',
+    ]
+]
+
+const zeroSolution = [
+    "#.##..##.",
+    "..#.##.#.",
+    "##..#...#",
+    "##...#..#",
+    "..#.##.#.",
+    "..##..##.",
+    "..#.##.#.",
+    ]
+
 describe("find correct mirrors of", () => {
     
     test("example1", () => {
         expect(solve(example1)).toBe(5);
     })
+    test("example1 with flips", () => {
+        expect(solution([example1]).solution2).toBe(300);
+    })
     test("example2", () => {
         expect(solve(example2)).toBe(400)
+    })
+    test("example2 with flips", () => {
+        expect(solution([example2]).solution2).toBe(100);
     })
     test("matrices[0]", () => {
         expect(solve(matrices[0])).toBe(8)
@@ -61,11 +127,33 @@ describe("diagonallyReflect", () => {
     })
 })
 
-describe("solution", () => {
-    test("is correct for examples", () => {
-        expect(solution([example1, example2])).toBe(405)
+describe("getFlipLocations", () => {
+    test("works correctly", () => {
+        const locations = [
+            [0, 0], [0, 2], [0, 3],
+            [1, 0], [1, 5], [1, 6]
+        ]
+        expect(getFlipLocations(smallExample).length).toBe(locations.length)
+        locations.map(location => {
+            expect(getFlipLocations(smallExample).toString().indexOf(location.toString())).not.toBe(-1)
+        })
     })
-    test("is correct for first matrix", () => {
-        expect(solution([matrices[0].slice()])).toBe(8)
+})
+
+describe("solution", () => {
+    test("1 is correct for examples", () => {
+        expect(solution([example1, example2]).solution1).toBe(405)
+    })
+    test("1 is correct for first matrix", () => {
+        expect(solution([matrices[0].slice()]).solution1).toBe(8)
+    })
+    test("2 is correct for examples", () => {
+        expect(solution([example1, example2]).solution2).toBe(400)
+    })
+    test("2 is correct for example set", () => {
+        expect(solution(exampleSet).solution2).toBe(1400)
+    })
+    test("1 is correct for the zero solution", () => {
+        expect(solution([zeroSolution]).solution1).toBe(0)
     })
 })
--- 2023/13/13.ts
+++ 2023/13/13.ts
@@ -11,12 +11,40 @@ for (const line of lines) {
     }
 }
 
-// matrices.map(m => {
-//     console.log()
-//     m.map(el => console.log(el))
-// })
+const neighbors = (coordinate: [number, number]): [number, number][] => {
+    return [coordinate, [coordinate[0], coordinate[1] + 1], [coordinate[0] + 1, coordinate[1]], [coordinate[0] + 1, coordinate[1] + 1]]
+}
 
-matrices[0].map(m => console.log(m))
+export const getFlipLocations = (matrix: string[]): [number, number][] => {
+    let locations: [number, number][] =[]
+    for (let i = 0; i < matrix.length - 1; i++) {
+        for (let j = 0; j < matrix[0].length - 1; j++) {
+            const neighbors = [matrix[i][j], matrix[i][j + 1], matrix[i + 1][j], matrix[i + 1][j + 1]];
+            (['#', '.']).map(c => {
+                if (neighbors.filter(el => el === c).length === 3) {
+                    locations.push([i, j])
+                }
+            })
+        }
+    }
+    return locations
+}
+
+export const flips = (matrix: string[], location: [number, number]): string[][] => {
+    const flip = c => c === '#' ? '.' : '#'
+    const toFlips = neighbors([location[0], location[1]])
+    let matrices: string[][] = []
+    toFlips.map(toFlip => {
+        let m = matrix.map(el => el.slice())
+        const row = matrix[toFlip[0]]
+        const cell = row[toFlip[1]]
+        const flippedCell = flip(cell)
+        const flippedRow = [...row.slice(0, toFlip[1]), flippedCell, ...row.slice(toFlip[1] + 1)].join("")
+        m[toFlip[0]] = flippedRow
+        matrices.push(m)
+    })
+    return matrices
+}
 
 export const rotate = (matrix: string[]): string[] => {
     let rotated = new Array(matrix[0].length).fill('')
@@ -28,8 +56,12 @@ export const rotate = (matrix: string[]): string[] => {
     return rotated
 }
 
-const solve = (matrix: string[], reflected = false, multiplier = 100): number => {
+const solve = (matrix: string[], reflected = false, rotated = false, ignore = NaN): number => {
+    const multiplier = rotated ? 1 : 100
     const testReflection = (matrix: string[]): boolean => {
+        if (matrix.length % 2 === 1) {
+            return false
+        }
         let i = 0;
         while (i < matrix.length / 2) {
             if (matrix[i] !== matrix[matrix.length - 1 - i]) {
@@ -46,28 +78,53 @@ const solve = (matrix: string[], reflected = false, multiplier = 100): number =>
             if (testReflection(matrix.slice(1, index))) {
                 const offset = reflected ? matrix.length - index : 1
                 const width = Math.floor(index / 2) 
-                return multiplier * (width + offset)
+                const solution = multiplier * (width + offset)
+                if (solution !== ignore) {
+                    return solution
+                }
             }
         }
         index += 1
         if (index === matrix.length - 1) {
             if (!reflected) {
                 const reversed = matrix.slice().reverse()
-                return solve(reversed, true, multiplier)
+                return solve(reversed, true, rotated, ignore)
             }
         }
     }
+    if (reflected && rotated) {
+        return 0
+    }
     const copy = matrix.slice()
-    return solve(rotate(copy), false, 1)
+    return solve(rotate(copy), false, true, ignore)
 }
 
-export const solution = (matrices: string[][]): number => {
-    return matrices.reduce((acc, matrix) => 
+export const solution = (matrices: string[][]): {solution1: number, solution2: number} => {
+    let solutions: number[] = []
+    const matrices1 = matrices.map(el => el.slice())
+    const solution1 = matrices1.reduce((acc, matrix, index) => 
     {
-        const solution = solve(matrix)
-        console.log(solution)
+        const solution = solve(matrix.slice())
+        solutions[index] = solution
         return acc + solution
     }, 0)
+
+    const matrices2 = matrices.map(el => el.slice())
+    const solution2 = matrices2.reduce((acc, matrix, index) => {
+        const previousSolution = solutions[index]
+        const flipLocations = getFlipLocations(matrix)
+        for (const location of flipLocations) {
+            for (const flip of flips(matrix.slice(), location)) {
+                const s = solve(flip, false, false, previousSolution)
+                if (s !== 0) {
+                    return acc + s
+                }
+            }
+        }
+        return acc
+    }, 0)
+
+    return {solution1, solution2}
 }
 
 console.log(solution(matrices))
